7d2043ba494d366fc4d738c27d6a71310c9442e5
viktoria <vik.slepchenko@gmail.com>
2021-01-25 23:09:18 +0100

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav>
+        <div class="nav">
+            <router-link to="/" class="nav__link">Список библиотек</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped lang="scss">
+    .nav {
+        height: 70px;
+        background-color: #1d1d1f;
+        width: 100%;
+        font-size: 25px;
+        padding-top: 18px;
+
+        &__link {
+            text-decoration: none;
+            color: rgb(245, 245, 247);
+            margin-left: 70px;
+            font-weight: 400;
+            font-style: normal;
+        }
+    }
+
+    @media (max-width: 500px) {
+        .nav {
+            &__link {
+                margin-left: 20px;
+            }
+        }
+    }
+</style>
---
+++ src/views/Library.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="library">
+        <p class="library__name">{{this.name}}</p>
+        <p class="library__description">{{this.description}}</p>
+        <div class="library__contacts">
+            <p><span>Телефон:</span> {{this.phone ? `+${this.phone}` : 'отсутствует'}}</p>
+            <p><span>Email:</span> {{this.email ? this.email : 'отсутствует'}}</p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import axios from "axios";
+
+    export default {
+        data() {
+            return {
+                description: '',
+                name: '',
+                email: '',
+                phone: ''
+            }
+        },
+        async created() {
+            try {
+                const res = await axios.get(`http://localhost:3000/libraries?_id=` +  this.$route.params.id)
+                this.description = res.data[0].data.general.description.replaceAll('<p>', '').replaceAll('</p>', '').replaceAll('<span>', '').replaceAll('</span>', '').replaceAll('<ul>', '').replaceAll('</ul>', '').replaceAll('<li>', '').replaceAll('</li>', '')
+                this.name = res.data[0].data.general.name
+                res.data[0].data.general.contacts.phones.length ? this.phone = res.data[0].data.general.contacts.phones[0].value : ''
+                res.data[0].data.general.contacts.email ? this.email = res.data[0].data.general.contacts.email : ''
+            } catch(e) {
+                console.error(e)
+            }
+        }
+    }
+</script>
+
+
+<style scoped lang="scss">
+    .library{
+        padding: 20px 70px;
+
+        &__name{
+            font-size: 24px;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        &__description{
+            font-size: 19px;
+        }
+
+        &__contacts{
+            font-size: 19px;
+
+            span{
+                font-weight: bold;
+            }
+        }
+    }
+
+    @media (max-width: 500px) {
+        .library{
+            padding: 20px;
+        }
+    }
+
+</style>
